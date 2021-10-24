3c5a44d0f32d7d493f93e1403e5dc62dd634bd81
1005281342 <1005281342@qq.com>
2021-10-24 21:02:51 +0800

    --story=888888888 test logger

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/1005281342/gozerorpcdemo
 go 1.15
 
 require (
+	github.com/1005281342/log v0.0.0-20211024120142-13970bb60ad1
 	github.com/tal-tech/go-zero v1.1.10
+	golang.org/x/sys v0.0.0-20211023085530-d6a326fbbf70 // indirect
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 )
--- internal/logic/pinglogic.go
+++ internal/logic/pinglogic.go
@@ -2,27 +2,55 @@ package logic
 
 import (
 	"context"
+	"time"
 
 	"github.com/1005281342/gozerorpcdemo/gozerorpcdemo"
 	"github.com/1005281342/gozerorpcdemo/internal/svc"
-
+	"github.com/1005281342/log"
 	"github.com/tal-tech/go-zero/core/logx"
 )
 
 type PingLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
-	logx.Logger
+}
+
+type ELKLogger struct {
+	log.Logger
+}
+
+func (e *ELKLogger) WithDuration(d time.Duration) logx.Logger {
+	e.Logger.WithDuration(d)
+	return e
 }
 
 func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
 	return &PingLogic{
 		ctx:    ctx,
 		svcCtx: svcCtx,
-		Logger: logx.WithContext(ctx),
+		//Logger: logx.WithContext(ctx),
 	}
 }
 
 func (l *PingLogic) Ping(in *gozerorpcdemo.Request) (*gozerorpcdemo.Response, error) {
-	return &gozerorpcdemo.Response{Pong: "OK. " + in.Ping}, nil
+	log.SetTraceID(l.ctx, "jesontest go-zero demo")
+	var (
+		logger         logx.Logger
+		loggerELK, err = log.WithContext(l.ctx,
+			log.WithAddress("127.0.0.1:5000"),
+			log.WithAppName("gozerorpcdemo"),
+			log.WithFuncName("Ping"),
+		)
+	)
+	if err != nil {
+		logger = logx.WithContext(l.ctx)
+		logger.Errorf("err: %+v", err)
+	} else {
+		logger = &ELKLogger{Logger: loggerELK}
+	}
+
+	var out = &gozerorpcdemo.Response{Pong: "OK. " + in.Ping}
+	logger.Infof("out: %+v", out)
+
+	return out, nil
 }
